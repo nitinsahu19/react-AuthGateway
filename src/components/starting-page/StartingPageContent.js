aab2efae1da51c922526c39cd81b8b363b854fef
nitinsahu19 <sahunitin900@gmail.com>
2023-12-15 15:50:06 +0530

    basic implementation of secure authentication portal built with React

---
+++ src/components/starting-page/StartingPageContent.js
@@ -0,0 +1,11 @@
+import classes from "./StartingPageContect.module.css";
+
+const StartingPageContent = () => {
+  return (
+    <section className={classes.starting}>
+      <h1>Welcome on Board!</h1>
+    </section>
+  );
+};
+
+export default StartingPageContent;
---
+++ src/pages/Homepage.js
@@ -0,0 +1,7 @@
+import StartingPageContent from "../components/starting-page/StartingPageContent";
+
+const HomePage = () => {
+  return <StartingPageContent />;
+};
+
+export default HomePage;
